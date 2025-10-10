2bc81c49bc52f27765e0277ccaceb45962251a36
Patrick Calahan <github@pcal.net>
2025-10-09 17:31:20 -0700

    update to minecraft 1.21.5

--- src/main/java/com/epherical/croptopia/common/MiscNames.java
+++ src/main/java/com/epherical/croptopia/common/MiscNames.java
@@ -3,5 +3,5 @@ package com.epherical.croptopia.common;
 public class MiscNames {
 
     public static final String MOD_ID = "croptopia";
-    public static final String INDEPENDENT_TAG = "${dependent}";
+    public static final String INDEPENDENT_TAG = "c";
 }
